0e09da889d712105510390465629c1b5d5853bf2
Matthew Nelson <05nelsonm@protonmail.com>
2019-09-18 19:29:25 -0400

    modified model to incorporate CsV into the project

--- M01/Assignment/app/src/main/java/com/lambdaschool/sharedpreferences/model/BookItem.kt
+++ M01/Assignment/app/src/main/java/com/lambdaschool/sharedpreferences/model/BookItem.kt
@@ -2,9 +2,39 @@ package com.lambdaschool.sharedpreferences.model
 
 import java.io.Serializable
 
-class BookItem(
+class BookItem /*(
     var name: String,
     var reason: String,
     var completed: Boolean,
     var id: Int
-) : Serializable
+
+)*/: Serializable {
+
+    companion object {
+        const val TAG = "BookItem"
+    }
+
+    var name: String? = null
+    var reason: String? = null
+    var completed: Boolean = false
+    var id: Int = 0
+
+    constructor(name: String?, reason: String?, completed: Boolean, id: Int) {
+        this.name = name
+        this.reason = reason
+        this.completed = completed
+        this.id = id
+    }
+    
+    constructor(csv: String) {
+        val item = csv.split(",")
+        this.name = item[0]
+        this.reason = item[1]
+        this.completed = item[2].toBoolean()
+        this.id = item[3].toInt()
+    }
+
+    fun createCsvString(): String {
+        return "$name,$reason,$completed,$id"
+    }
+}
