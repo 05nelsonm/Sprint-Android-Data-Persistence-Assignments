eaea7e02722a1ee7f64487ac081e629a0eddf77d
Matthew Nelson <05nelsonm@protonmail.com>
2019-09-19 20:14:31 -0400

    built out DAO

--- M04/Assignment/app/src/main/java/com/lambdaschool/sharedpreferencesm04/database/BookEntryDAO.kt
+++ M04/Assignment/app/src/main/java/com/lambdaschool/sharedpreferencesm04/database/BookEntryDAO.kt
@@ -1,4 +1,25 @@
 package com.lambdaschool.sharedpreferencesm04.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.lambdaschool.sharedpreferencesm04.model.BookItem
+
+@Dao
 interface BookEntryDAO {
+
+    // TODO 10: Insert with onConflict = REPLACE
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    fun createEntry(entry: BookItem)
+
+    // TODO 11: Query for all entities
+    @Query("SELECT * FROM bookitem")
+    fun readAllEntries(): LiveData<List<BookItem>> // TODO 27: Return LiveData for VM
+
+    // TODO 12: Update with onConflict = REPLACE
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun updateEntry(entry: BookItem)
+
+    // TODO 13: DELETE
+    @Delete
+    fun deleteEntry(entry: BookItem)
 }
