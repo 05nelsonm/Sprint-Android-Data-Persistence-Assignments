5bc2a8f9aa06fbd6d5b7d347a04f5a43afddb7da
Matthew Nelson <05nelsonm@protonmail.com>
2019-09-19 20:15:11 -0400

    built out DB repo

--- M04/Assignment/app/src/main/java/com/lambdaschool/sharedpreferencesm04/database/BookDBRepo.kt
+++ M04/Assignment/app/src/main/java/com/lambdaschool/sharedpreferencesm04/database/BookDBRepo.kt
@@ -1,4 +1,34 @@
 package com.lambdaschool.sharedpreferencesm04.database
 
-class BookDBRepo {
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+import com.lambdaschool.sharedpreferencesm04.model.BookItem
+import com.lambdaschool.sharedpreferencesm04.util.BookItemRepoInterface
+
+class BookDBRepo(val context: Context) : BookItemRepoInterface {
+
+    override fun createEntry(entry: BookItem) {
+        database.bookEntryDao().createEntry(entry)
+    }
+
+    override fun readAllEntries(): LiveData<List<BookItem>> {
+        return database.bookEntryDao().readAllEntries()
+    }
+
+    override fun updateEntry(entry: BookItem) {
+        database.bookEntryDao().updateEntry(entry)
+    }
+
+    override fun deleteEntry(entry: BookItem) {
+        database.bookEntryDao().deleteEntry(entry)
+    }
+
+    // TODO 15: Build the Room database
+    private val database by lazy {
+        Room.databaseBuilder(
+            context.applicationContext,
+            BookEntryDB::class.java, "entry_database"
+        ).fallbackToDestructiveMigration().build()
+    }
 }
